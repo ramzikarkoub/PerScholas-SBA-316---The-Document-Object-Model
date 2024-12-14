bb7e2c0811e2f640e9907abb23c5fe0aa44f9a66
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2024-12-14 15:18:29 -0500

    added comparising function

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const gameHistory = document.querySelector("#game-history");
 const form = document.querySelector("#feedback-form");
 const formMessage = document.querySelector("#form-message");
 const moves = ["Rock", "Paper", "Scissors"];
+let userScore = 0;
+let computerScore = 0;
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
@@ -12,17 +14,45 @@ buttons.forEach((button) => {
     console.log(userChoice);
     const computerChoice = getComputerChoice();
     console.log(computerChoice);
-    if (userChoice == "Rock" && computerChoice == "Paper") {
-      console.log("you choosed rock and the computer paper");
-    } else if (userChoice == "Rock" && computerChoice == "Scissors") {
-      console.log("hahahah");
-    } else {
-      console.log("meaw");
-    }
+    const comparisonChoice = determineWinner(userChoice, computerChoice);
+    messageReturned(comparisonChoice);
+    console.log("hahahahahhahahahahah", userScore);
+    console.log("hihihihihihihihihihi", computerScore);
   });
 });
 const getComputerChoice = () => {
   return moves[Math.floor(Math.random() * moves.length)];
 };
 
+function determineWinner(player, computer) {
+  if (player === computer) {
+    console.log("tie");
+    return "It's a tie!";
+  }
+  if (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Paper" && computer === "Rock") ||
+    (player === "Scissors" && computer === "Paper")
+  ) {
+    console.log("you win");
+    return "You win!";
+  }
+  console.log("you lose");
+  return "You lose!";
+}
 // console.log(getComputerChoice());
+console.log(userScore);
+console.log(computerScore);
+
+function messageReturned(comparisonChoice) {
+  if (comparisonChoice == "It's a tie!") {
+    console.log("tie");
+  } else if (comparisonChoice == "You win!") {
+    console.log("you win");
+    return (userScore += 1);
+  } else {
+    console.log("computer win");
+    computerScore += 1;
+    return computerScore;
+  }
+}
