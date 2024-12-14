71024005b3398c765f4a3064ff137b3dbf93dd4d
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2024-12-14 14:38:19 -0500

    grabbed the elements in js file

--- app.js
+++ app.js
@@ -4,9 +4,3 @@ const resultDisplay = document.querySelector("#game-result");
 const gameHistory = document.querySelector("#game-history");
 const form = document.querySelector("#feedback-form");
 const formMessage = document.querySelector("#form-message");
-
-buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    console.log("objectdsdsd");
-  });
-});
