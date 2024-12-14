084dfa127604f2d57834e8bca870ef4052c92a4e
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2024-12-14 14:37:11 -0500

    finished index.html

---
+++ app.js
@@ -0,0 +1,12 @@
+// Cache DOM elements
+const buttons = document.querySelectorAll(".move-btn");
+const resultDisplay = document.querySelector("#game-result");
+const gameHistory = document.querySelector("#game-history");
+const form = document.querySelector("#feedback-form");
+const formMessage = document.querySelector("#form-message");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log("objectdsdsd");
+  });
+});
