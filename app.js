4f5cecbb4252eb2bd959092580751b550ad71363
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2024-12-14 14:38:54 -0500

    did the foreach function for the buttons

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const resultDisplay = document.querySelector("#game-result");
 const gameHistory = document.querySelector("#game-history");
 const form = document.querySelector("#feedback-form");
 const formMessage = document.querySelector("#form-message");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log("objectdsdsd");
+  });
+});
