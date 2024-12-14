84437ac4dfdd90ae817ecc64f366994143aa8f10
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2024-12-14 14:46:59 -0500

    generate computer random choice

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ const resultDisplay = document.querySelector("#game-result");
 const gameHistory = document.querySelector("#game-history");
 const form = document.querySelector("#feedback-form");
 const formMessage = document.querySelector("#form-message");
+const moves = ["rock", "paper", "scissors"];
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    console.log(e.target.textContent);
+    const userChoice = e.target.textContent;
+    console.log(userChoice);
+    console.log(getComputerChoice());
   });
 });
+const getComputerChoice = () => {
+  return moves[Math.floor(Math.random() * moves.length)];
+};
+
+// console.log(getComputerChoice());
